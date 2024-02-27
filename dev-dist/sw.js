3cfdc10d8876ba5b05ce230d894ac2926c7bfd44
eoFranca <erfranca.com@gmail.com>
2024-02-27 14:47:30 -0300

    Menu superior

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.db18h6qi4gg"
+    "revision": "0.aqnlrknkap8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
